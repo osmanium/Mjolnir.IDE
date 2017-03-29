ef33c74e02e1d9b277f6cec7da15904af985bba1
Osman GUMUS <osmangumus@outlook.com>
2017-03-30 00:47:28 +0400

    first White ui test

---
+++ Mjolnir.IDE/Mjolnir.IDE.Test.UITest/SmokeTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using TestStack.White;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+using Xunit;
+
+namespace Mjolnir.IDE.Test.UITest
+{
+    public class SmokeTest
+    {
+        [Fact]
+        public void should_open_ide()
+        {
+            Application application = Application.Launch(@"C:\Prj\GitHub\Mjolnir.IDE\Mjolnir.IDE\Mjolnir.IDE.Test\bin\Debug\Mjolnir.IDE.Test.exe");
+            Window window = application.GetWindow("Mjolnir.IDE");
+
+            var menuBar = window.MenuBar;
+            TestStack.White.UIItems.MenuItems.Menu selectFileOpen = menuBar.MenuItem("File", "New");
+            selectFileOpen.Click();
+
+            application.Close();
+        }
+    }
+}
